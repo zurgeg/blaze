2cc8773201a334a459679f973b55fa1d9d5e0ed2
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-01-30 15:18:02 -0500

    Update libemu.py

--- blazelib/libemu.py
+++ blazelib/libemu.py
@@ -28,7 +28,7 @@ def exec_rom(file, bytes_per_instruction, console):
         console_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(console_module)
         # Now we are done that, and we can read 4 bytes
-        data = file.read(n_bytes)
+        data = file.read(bytes_per_instruction)
         while data:
             data = str(hexlify(data))[2:-1]
             # All done loading data! Now we can call upon our "trusty" partner... RegEx!
@@ -43,7 +43,7 @@ def exec_rom(file, bytes_per_instruction, console):
                 statement = re.compile(list(i.keys())[0])
                 if statement.match(data):
                     console_module.__getattr__(list(i.values()[0]))() # note: this line is spaghetti, please fix thx
-            data = file.read(n_bytes)
+            data = file.read(bytes_per_instruction)
 
     
         
