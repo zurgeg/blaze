48342819de971ba8de5daf0d5400acc1f2bea5ed
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-01-30 11:41:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import blazelib.core
 import blazelib.libemu
 console = blazelib.core.load_console()
+# NOTE: somerom.rom is a ROM of Techmo Super Bowl. Get it yourself because it's not here and we don't encourage piracy
 blazelib.libemu.read_and_exec('somerom.rom', 20745, console)
 
