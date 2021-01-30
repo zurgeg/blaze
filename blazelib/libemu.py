2dd3fbeb1b55d2bb1752b2823139163605dbe4b7
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-01-30 10:13:11 -0500

    Create libemu.py

---
+++ blazelib/libemu.py
@@ -0,0 +1,28 @@
+import importlib.util
+from binascii import hexlify
+import re, json
+def read_and_exec(file, offset, console):
+    with file as open(file):
+        spec = importlib.util.spec_from_file_location("console", console[0])
+        console_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(console_module)
+        # Now we are done that, and we can read 4 bytes
+        data = []
+        data.append(file.read(offset))
+        data.append(file.read(offset + 1))
+        data.append(file.read(offset + 2))
+        data.append(file.read(offset + 3))
+        data = binascii.hexlify(''.join(data))
+        # All done loading data! Now we can call upon our "trusty" partner... RegEx!
+        
+        
+        
+        # just kidding i hate regex
+        # I just realized we now have to compile every statement in the spec file! *super mario 64 slide music plays*
+        # yeeeeeah, probably should've done that beforehand somehow
+        spec = json.load(console[1])
+        for i in spec['patterns']:
+            raise NotImplementedError
+        
+        
+        
