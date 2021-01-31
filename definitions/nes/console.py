06837c72672fe585d03e8815d9f65c28e98a83b7
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-01-31 09:03:07 -0500

    Update console.py

--- definitions/nes/console.py
+++ definitions/nes/console.py
@@ -6,4 +6,4 @@ def ora(_):
     print('ORA instruction')
 def stp(_):
     print('STP instruction. Stop maybe?')
-    raise STPException('Encountered a stop instruction, halt')
+    raise STPException
