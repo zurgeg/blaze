6a83be55ddf3a90b162a5f601fc9d35651e0d6cf
zurgeg <zurgeg@gmail.com>
2021-01-31 17:54:44 -0500

    Update libemu stuff, will be followed by a NES console update

--- blazelib/libemu.py
+++ blazelib/libemu.py
@@ -4,7 +4,13 @@ import re, json
 instruction_cache = {} # This caches instructions
 # Format: {offset: function}
 # TODO: Make exec_rom take advantage of this
+current_rom = ''
+current_console = []
+current_addr = None
+base = 0x0
 def read_and_exec(file, offset, n_bytes, console):
+    global base
+    base += offset
     with open(file, 'rb') as file:
         spec = importlib.util.spec_from_file_location("console", f"{console[3]}/{console[0]}")
         console_module = importlib.util.module_from_spec(spec)
@@ -23,15 +29,21 @@ def read_and_exec(file, offset, n_bytes, console):
         spec = console[1]
         for i in spec['patterns']:
             statement = re.compile(list(i.keys())[0])
+            number_of_arguments = i['args']
             if statement.match(data):
-                getattr(console_module, list(i.values())[0])() # note: this line is spaghetti, please fix thx
+                arguments = hexlify(file.read(number_of_arguments))
+                getattr(console_module, list(i.values())[0])(arguments) # note: this line is spaghetti, please fix thx
 def exec_rom(file, bytes_per_instruction, console):
+    global current_rom, current_console, current_addr
+    current_rom = file
+    current_console = console
     with open(file, 'rb') as file:
         spec = importlib.util.spec_from_file_location("console", f"{console[3]}/{console[0]}")
         console_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(console_module)
         # Now we are done that, and we can read 4 bytes
-        data = file.read(bytes_per_instruction)
+        data = file.read(base + bytes_per_instruction)
+        current_addr = bytes_per_instruction
         while data:
             data = str(hexlify(data))[2:-1]
             # All done loading data! Now we can call upon our "trusty" partner... RegEx!
@@ -47,8 +59,12 @@ def exec_rom(file, bytes_per_instruction, console):
                 number_of_arguments = i['args']
                 if statement.match(data):
                     arguments = hexlify(file.read(number_of_arguments))
-                    getattr(console_module, list(i.values())[0])(arguments) # note: this line is spaghetti, please fix thx
-            data = file.read(bytes_per_instruction)
+                    if "kwargs" in list(i.keys()):   
+                        getattr(console_module, list(i.values())[0])(arguments, **spec['kwargs']) # note: this line is spaghetti, please fix thx
+                    else:
+                        getattr(console_module, list(i.values())[0])(arguments) # 
+            data = file.read(base + bytes_per_instruction)
+            current_addr = base + current_addr + bytes_per_instruction
 
     
         
