c7dc066e2fb4d7fe41482ecbdcd4bef1a45c1d5c
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-02-07 10:48:17 -0500

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,4 @@ mkdir bin
 pip3 install nuitka
 python -m nuitka --standalone cli.py
 mv cli.dist/cli cli.dist/blaze_cli
-zip -r cli.dist.zip cli.dist
-mv cli.dist.zip bin/linux_nightly.zip
-rm -rf cli.dist/
+
