311d28f05414e97db3474caa76b2c37bd804e573
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-02-07 10:39:24 -0500

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -2,6 +2,6 @@ mkdir bin
 pip install nuitka
 nuitka --standalone cli.py --assume-yes-for-downloads
 move cli.dist/cli.exe cli.dist/blaze_cli.exe
-powershell Compress-Archive cli.dist bin/windows_nightly.zip
+tar.exe -a -c -f bin/windows_nightly.zip cli.dist
 del cli.dist/*
 
