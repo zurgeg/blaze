d8d6fa7c73e7129ada4e2dacada150b1363255bf
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-02-08 15:54:59 -0500

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,6 @@
 mkdir bin
 pip install nuitka
 nuitka --standalone cli.py --assume-yes-for-downloads
+nuitka --standalone gui.py --assume-yes-for-downloads
 move cli.dist/cli.exe cli.dist/blaze_cli.exe
-
+move cli.dist/cli.exe gui.dist/blaze_gui.exe
